ac0d02da2699536fe46dc5f9e7616aff53e31739
Sheikh Hafsa Nadeem <124384550+hafsa-nadim@users.noreply.github.com>
2023-04-04 09:26:42 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+  // Qno.01
+ var userName;
+
+ // Qno.02
+let myName = "Sheikh Hafsa Nadim";
+
+ // Qno.03
+let message = "Hello World!"
+alert(message)
+
+ // Qno.04
+let name = "Jhone Doe";
+let age = "15 Years old";
+let certificate = "Certified Mob Application Development";
+alert(name);
+alert(age);
+alert(certificate);
+
+// Qno.05
+let name1 = "PIZZA";
+alert(name1);
+alert(name1.food(0, name1.length-1));
+alert(name1.food(0, name1.length-2));
+alert(name1.food(0, name1.length-3));
+alert(name1.food(0, name1.length-4));
+
+ // Qno.06
+let email = "hafsanadim246@gmail.com";
+alert("My email address is " + email);
+
+ // Qno.07
+let book = "A Smarter Way to Learn JavaScript";
+alert("I am trying to learn from the book " + book);
+
+ // Qno.08
+document.write("Yah!I can write HTML content through JavaScript.");
+
+ // Qno.09
+let symbols = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(symbols);
